701493e36c3fd0b103eed1f892bcdafb711e1087
Ju Isaeva <firenaack@gmail.com>
2022-05-16 10:56:46 +0100

    Initialise repository

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# pytest_examples
+# test examples
+
+Test 0: Initialise the repository\
+Test 1: Adding extra validation/assertion to an existing scenario\
+Test 2: Fix an existing broken scenario\
+Test 3: Add new step definition to an existing scenario
---
+++ mytest.py
@@ -0,0 +1,25 @@
+import json
+import pathlib
+
+
+def pytest_generate_tests(metafunc):
+    row_data = pathlib.Path(metafunc.module.__file__).with_name('data.json')
+    test_data = json.loads(row_data.read_text())
+
+    arg_values = []
+    arg_names = []
+    id_list = []
+
+    for scenario in test_data:
+        id_list.append(scenario.get("id"))
+        items = scenario.get("data").items()
+        arg_names = [x[0] for x in items]
+        arg_values.append([x[1] for x in items])
+    metafunc.parametrize(arg_names, arg_values, ids=id_list, scope="class")
+
+
+class TestSampleWithScenarios:
+
+    def test_check_data_types(self, params, result):
+        assert isinstance(params, list), "Params type is not list"
+
