e70f979a91957d914625ef3c0ad9eab4392a6b45
Ju Isaeva <firenaack@gmail.com>
2022-05-16 11:07:37 +0100

    Test 1: Adding extra validation/assertion to an existing scenario. Adding data that will fail test

--- mytest.py
+++ mytest.py
@@ -22,4 +22,5 @@ class TestSampleWithScenarios:
 
     def test_check_data_types(self, params, result):
         assert isinstance(params, list), "Params type is not list"
+        assert isinstance(result, int), "Result is not empty"  # Adding extra assertion to an existing scenario
 
